c1349ed497a1f56eba8a6bbdefaae1ddfc505cbf
Dinara275 <dinara.akhmetzhanova@gmail.com>
2023-05-20 19:31:42 +0300

    modify Makefile, brain-games, Cli.php, composer.json, composer.lock, add bin/brain-even

--- src/Cli.php
+++ src/Cli.php
@@ -1,8 +1,17 @@
-namespace BrainGames\Cli
+<?php
 
+namespace src\Cli;
+
+require_once __DIR__ . '/../vendor/autoload.php';
 use function cli\line;
 use function cli\prompt;
 
-line("Welcome to the Brain Games!");
-$name = prompt("May I have your name?");
-line("Hello, %s!", $name);
+if (!function_exists('welcome')) {
+function welcome($name)
+{
+	line("Welcome to the Brain Games!");
+	$name = prompt("May I have your name?");
+	line("Hello, %s!", $name);
+}
+}
+
