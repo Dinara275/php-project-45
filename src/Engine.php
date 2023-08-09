76a0ff2c7c6167eed6020c769e19e2e1c24eedcd
Dinara275 <dinara.akhmetzhanova@gmail.com>
2023-08-09 18:54:34 +0300

    modify Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -5,7 +5,6 @@ namespace BrainGames\Engine;
 use function cli\line;
 use function cli\prompt;
 
-
 const ROUNDS = 3;
 
 function start(string $gamePurpose, callable $getRound)
@@ -18,10 +17,8 @@ function start(string $gamePurpose, callable $getRound)
 
     for ($i = 0; $i < ROUNDS; $i++) {
         [$answer, $correctAnswer] = $getRound();
-	
 	line("Your answer: {$answer}");
-	
-	if ($answer !== $correctAnswer) {
+        if ($answer !== $correctAnswer) {
             line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
             line("Let's try again, %s!", $name);
             return;
